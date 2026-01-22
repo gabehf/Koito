08fc9eed86d657bab524c4a52cb8b4569df172b4
Gabe Farrell <90876006+gabehf@users.noreply.github.com>
2026-01-22 17:01:46 -0500

    fix: correct interest bucket queries (#169)

--- internal/db/psql/interest.go
+++ internal/db/psql/interest.go
@@ -14,54 +14,54 @@ func (d *Psql) GetInterest(ctx context.Context, opts db.GetInterestOpts) ([]db.I
 		return nil, errors.New("GetInterest: bucket count must be provided")
 	}
 
-	ret := make([]db.InterestBucket, opts.Buckets)
+	ret := make([]db.InterestBucket, 0)
 
 	if opts.ArtistID != 0 {
 		resp, err := d.q.GetGroupedListensFromArtist(ctx, repository.GetGroupedListensFromArtistParams{
 			ArtistID:    opts.ArtistID,
-			BucketCount: opts.Buckets,
+			BucketCount: int32(opts.Buckets),
 		})
 		if err != nil {
 			return nil, fmt.Errorf("GetInterest: GetGroupedListensFromArtist: %w", err)
 		}
-		for i, v := range resp {
-			ret[i] = db.InterestBucket{
+		for _, v := range resp {
+			ret = append(ret, db.InterestBucket{
 				BucketStart: v.BucketStart,
 				BucketEnd:   v.BucketEnd,
 				ListenCount: v.ListenCount,
-			}
+			})
 		}
 		return ret, nil
 	} else if opts.AlbumID != 0 {
 		resp, err := d.q.GetGroupedListensFromRelease(ctx, repository.GetGroupedListensFromReleaseParams{
 			ReleaseID:   opts.AlbumID,
-			BucketCount: opts.Buckets,
+			BucketCount: int32(opts.Buckets),
 		})
 		if err != nil {
 			return nil, fmt.Errorf("GetInterest: GetGroupedListensFromRelease: %w", err)
 		}
-		for i, v := range resp {
-			ret[i] = db.InterestBucket{
+		for _, v := range resp {
+			ret = append(ret, db.InterestBucket{
 				BucketStart: v.BucketStart,
 				BucketEnd:   v.BucketEnd,
 				ListenCount: v.ListenCount,
-			}
+			})
 		}
 		return ret, nil
 	} else if opts.TrackID != 0 {
 		resp, err := d.q.GetGroupedListensFromTrack(ctx, repository.GetGroupedListensFromTrackParams{
 			ID:          opts.TrackID,
-			BucketCount: opts.Buckets,
+			BucketCount: int32(opts.Buckets),
 		})
 		if err != nil {
 			return nil, fmt.Errorf("GetInterest: GetGroupedListensFromTrack: %w", err)
 		}
-		for i, v := range resp {
-			ret[i] = db.InterestBucket{
+		for _, v := range resp {
+			ret = append(ret, db.InterestBucket{
 				BucketStart: v.BucketStart,
 				BucketEnd:   v.BucketEnd,
 				ListenCount: v.ListenCount,
-			}
+			})
 		}
 		return ret, nil
 	} else {
