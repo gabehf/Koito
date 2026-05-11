b278e414241d13cc5cfaca673365c253d2f094d5
Gabe Farrell <gabe@mnrva.dev>
2026-05-10 20:08:50 -0400

    feat: version checker and notification dot

--- client/app/components/sidebar/SidebarSettings.tsx
+++ client/app/components/sidebar/SidebarSettings.tsx
@@ -2,36 +2,47 @@ import { Settings2 } from "lucide-react";
 import SettingsModal from "../modals/SettingsModal";
 import SidebarItem from "./SidebarItem";
 import { useEffect, useState } from "react";
+import { useAppContext } from "~/providers/AppProvider";
 
 interface Props {
-    size: number
+  size: number;
 }
 
 export default function SidebarSettings({ size }: Props) {
-    const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(false);
+  const { updateAvailable } = useAppContext();
 
-    useEffect(() => {
-        const handleKeyDown = (e: KeyboardEvent) => {
-            const active = document.activeElement;
-            const isTyping = active && (
-                active.tagName === 'INPUT' ||
-                active.tagName === 'TEXTAREA' ||
-                (active as HTMLElement).isContentEditable
-            );
-    
-            if (!isTyping && e.key === '\\') {
-                e.preventDefault();
-                setOpen(!open);
-            }
-        };
-    
-        document.addEventListener('keydown', handleKeyDown);
-        return () => document.removeEventListener('keydown', handleKeyDown);
-    }, [open]);
+  useEffect(() => {
+    const handleKeyDown = (e: KeyboardEvent) => {
+      const active = document.activeElement;
+      const isTyping =
+        active &&
+        (active.tagName === "INPUT" ||
+          active.tagName === "TEXTAREA" ||
+          (active as HTMLElement).isContentEditable);
 
-    return (
-        <SidebarItem space={30} keyHint="\" name="Settings" onClick={() => setOpen(true)} modal={<SettingsModal open={open} setOpen={setOpen} />}>
-            <Settings2 size={size} />
-        </SidebarItem>
-    )
+      if (!isTyping && e.key === "\\") {
+        e.preventDefault();
+        setOpen(!open);
+      }
+    };
+
+    document.addEventListener("keydown", handleKeyDown);
+    return () => document.removeEventListener("keydown", handleKeyDown);
+  }, [open]);
+
+  return (
+    <SidebarItem
+      space={30}
+      keyHint="\"
+      name="Settings"
+      onClick={() => setOpen(true)}
+      modal={<SettingsModal open={open} setOpen={setOpen} />}
+    >
+      <Settings2 size={size} />
+      {updateAvailable && (
+        <div className="h-1.5 w-1.5 rounded-full bg-(--color-info) absolute top-1 right-1" />
+      )}
+    </SidebarItem>
+  );
 }
