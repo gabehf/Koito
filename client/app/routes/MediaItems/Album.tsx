f47fd097e05b68244cdad07d2c1506d27cb7fa26
Gabe Farrell <gabe@mnrva.dev>
2026-05-13 12:13:23 -0400

    media item layout

--- client/app/routes/MediaItems/Album.tsx
+++ client/app/routes/MediaItems/Album.tsx
@@ -69,7 +69,7 @@ export default function Album() {
       }
     >
       <div className="flex flex-col gap-20">
-        <div className="flex gap-10 md:gap-20 mt-10 flex-wrap items-center">
+        <div className="flex gap-10 md:gap-20 mt-10 flex-wrap items-center max-w-[1400px]">
           <div className="flex gap-10 md:gap-25 flex-wrap lg:flex-nowrap items-start">
             <TopTracks limit={8} period={period} albumId={album.id} />
             <div className="min-w-[350px] w-2/5">
